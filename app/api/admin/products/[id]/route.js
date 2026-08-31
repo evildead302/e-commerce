84439f08bf7d4afcba3dbd2d9d9d41c6e7af0381
evildead302 <jonepeter672@gmail.com>
2026-08-31 21:56:51 +0500

    Update route.js

--- app/api/admin/products/[id]/route.js
+++ app/api/admin/products/[id]/route.js
@@ -7,10 +7,7 @@
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request, { params }) {
   try {
     const product = await prisma.product.findUnique({
       where: { id: params.id }
